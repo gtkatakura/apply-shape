b48033d5b8bc10a028b95a81c5fa6046204d5565
gtkatakura <gt.katakura@gmail.com>
2018-10-13 20:24:06 -0300

    feat(core): support for mapping values, arrays and objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-const applyShape = () => {}
+const isObject = (value) => value !== null && typeof value === 'object'
+
+const isArray = (value) => value instanceof Array
+
+const isFunction = (value) => value instanceof Function
+
+const fromEntries = (entries) => entries.reduce((reduced, [key, value]) => {
+  reduced[key] = value
+  return reduced
+}, {})
+
+const mapValues = (transformation, object) => {
+  if (isArray(object)) {
+    return object.map(transformation)
+  }
+
+  const entries = Object.entries(object).map(([key, value]) => {
+    return [key, transformation(value, key)]
+  })
+
+  return fromEntries(entries)
+}
+
+const applyShape = (shape, object) => {
+  if (isObject(object)) {
+    return mapValues((value, key) => applyShape(shape[key], value), object)
+  }
+
+  if (isFunction(shape)) {
+    return shape(object)
+  }
+
+  return object
+}
 
 export default applyShape
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,86 @@
 import applyShape from '../src'
 
 describe('applyShape', () => {
+  const inc = (value) => value + 1
+
   test('is a function', () => {
     expect(applyShape).toBeInstanceOf(Function)
+  })
+
+  test('(inc, 100) => 101', () => {
+    expect(applyShape(inc, 100)).toEqual(101)
+  })
+
+  test('({ value: inc }, { value: 1 }) => { value: 2 }', () => {
+    const shape = {
+      value: inc,
+    }
+
+    const object = {
+      value: 1,
+    }
+
+    expect(applyShape(shape, object)).toEqual({
+      value: 2,
+    })
+  })
+
+  test('({ value: inc }, { value: 1, extra: 1 }) => { value: 2, extra: 1 }', () => {
+    const shape = {
+      value: inc,
+    }
+
+    const object = {
+      value: 1,
+      extra: 1,
+    }
+
+    expect(applyShape(shape, object)).toEqual({
+      value: 2,
+      extra: 1,
+    })
+  })
+
+  test('({ context: { value: inc } }, { context: { value: 1, name: "extra" } }) => { context: { value: 2, name: "extra" } }', () => {
+    const shape = {
+      context: {
+        value: inc,
+      },
+    }
+
+    const object = {
+      context: {
+        value: 1,
+        name: 'extra'
+      },
+    }
+
+    expect(applyShape(shape, object)).toEqual({
+      context: {
+        value: 2,
+        name: "extra",
+      },
+    })
+  })
+
+  test('([inc, inc], [1, 1]) => [2, 2]', () => {
+    const shape = [inc, inc]
+    const object = [1, 1]
+
+    expect(applyShape(shape, object)).toEqual([2, 2])
+  })
+
+  test('([inc], [1, 100, -100]) => [2, 100, -100]', () => {
+    const shape = [inc]
+    const object = [1, 100, -100]
+
+    expect(applyShape(shape, object)).toEqual([2, 100, -100])
+  })
+
+  test('([{ value: inc }], [{ value: 0 }, "extra"]) => [{ value: 1 }, "extra"]', () => {
+    const shape = [{ value: inc }]
+    const object = [{ value: 0 }, "extra"]
+
+    expect(applyShape(shape, object)).toEqual([{ value: 1 }, "extra"])
   })
 })
