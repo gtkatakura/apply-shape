a558764400a6ac793e2a8f1f4dd33a26d1388907
gtkatakura <gt.katakura@gmail.com>
2018-10-12 22:08:04 -0300

    feat(bootstrap): configured babel to build cjs+esm, jest to test

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const applyShape = () => {}
+
+export default applyShape
---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+import applyShape from '../src'
+
+describe('applyShape', () => {
+  test('is a function', () => {
+    expect(applyShape).toBeInstanceOf(Function)
+  })
+})
